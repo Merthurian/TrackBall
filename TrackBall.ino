2b72dfa43ac446dc9c1baa33b6f4eb4950a2c201
Dan Purcell <merthurian@hotmail.com>
2017-10-21 19:02:56 +0100

    It works.

--- TrackBall.ino
+++ TrackBall.ino
@@ -6,40 +6,65 @@ Track Ball
 #define ENCODER_OPTIMIZE_INTERRUPTS
 #include <Encoder.h>
 
-//Attach X & Y to interrupt pins
+//Attach to interrupt pins
 Encoder xEncoder(2, 3);
-Encoder yEncoder(18, 19);
+//Encoder yEncoder(18, 19);
+
+const int LED_PIN = 52;
+const int BUTTON_PIN = 53;
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  digitalWrite(LED_PIN, LOW);
   Serial.begin(115200);
-  Serial.println("Track Ball");
 }
 
 long xPosition  = -999;
-long yPosition = -999;
+//long yPosition = -999;
 
-unsigned long time;
+unsigned long nextTrackTime;
+unsigned long ledTime;
+
+boolean buttonState = false;
 
 void loop() {  
-  if(millis() > time){
-    time = millis() + 20;
+  buttonState = !digitalRead(BUTTON_PIN);
+  
+  if(millis() > nextTrackTime){
+    nextTrackTime = millis() + 20;
     
-    Serial.print("0");
-    
-    long x = xEncoder.read();
-    
-    char sign = (x > -1) ? '+' : '-';
-    
+    Serial.print("0");    
+    long x = xEncoder.read()/2; //TODO: Make this better.
+    char sign = (x > -1) ? '+' : '-';    
     Serial.print(sign);
     
     padding(abs(x), 4);
     
     Serial.print("\r\n");
     
-    xEncoder.write(0);
+    xEncoder.write(0); //Reset the encoder count
+  }
+  
+  if (Serial.available() > 0) {
+    if (Serial.read() == '1') {
+      digitalWrite(LED_PIN, HIGH);
+      ledTime = millis() + 500;
+    }      
+  }
+  
+  if (millis() > ledTime)
+    digitalWrite(LED_PIN, LOW);
+  else {
+    if (buttonState) {
+      delay(20);
+      Serial.println("100000");
+      delay(10000);
+    }
   }
 }
 
+//zero pad and limit digits
 void padding( int number, byte width ) {
  int currentMax = 10;
  for (byte i=1; i<width; i++){
